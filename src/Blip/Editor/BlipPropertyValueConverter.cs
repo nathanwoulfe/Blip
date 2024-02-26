f9948c21de776868bdd854313491f82a940c2c49
Laura Weatherhead <lssweatherhead@googlemail.com>
2024-02-26 04:02:23 +0000

    Add conversion support for content delivery API (#5)
    * Add conversion support for content delivery API
    
    * Refactor based on code recomms

--- src/Blip/Editor/BlipPropertyValueConverter.cs
+++ src/Blip/Editor/BlipPropertyValueConverter.cs
@@ -1,19 +1,24 @@
 using Newtonsoft.Json;
+using Umbraco.Cms.Core.DeliveryApi;
 using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.DeliveryApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.PropertyEditors.DeliveryApi;
 using Umbraco.Cms.Core.PublishedCache;
 using Umbraco.Extensions;
 
 namespace Blip.Editor;
 
-public class BlipPropertyValueConverter : IPropertyValueConverter
+public class BlipPropertyValueConverter : IPropertyValueConverter, IDeliveryApiPropertyValueConverter
 {
     private readonly IPublishedSnapshotAccessor _publishedSnapshotAccessor;
+    private readonly IApiElementBuilder _apiElementBuilder;
 
-    public BlipPropertyValueConverter(IPublishedSnapshotAccessor publishedSnapshotAccessor)
+    public BlipPropertyValueConverter(IPublishedSnapshotAccessor publishedSnapshotAccessor, IApiElementBuilder apiElementBuilder)
     {
         _publishedSnapshotAccessor = publishedSnapshotAccessor;
+        _apiElementBuilder = apiElementBuilder;
     }
 
     public object? ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview)
@@ -61,17 +66,23 @@ public class BlipPropertyValueConverter : IPropertyValueConverter
         return new BlockListModel(blocks.ToList());
     }
 
+    /// <inheritdoc />
     public object? ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview)
         => inter?.ToString() ?? null;
 
+    /// <inheritdoc />
     public object? ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object? source, bool preview) => source;
 
+    /// <inheritdoc/>
     public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Elements;
 
+    /// <inheritdoc />
     public Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(BlockListModel);
 
+    /// <inheritdoc />
     public bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias.ToLower().Equals("nw.blip");
 
+    /// <inheritdoc />
     public bool? IsValue(object? value, PropertyValueLevel level)
         => level switch
         {
@@ -80,4 +91,36 @@ public class BlipPropertyValueConverter : IPropertyValueConverter
             PropertyValueLevel.Object => null,
             _ => throw new NotSupportedException($"Invalid level: {level}."),
         };
+
+    /// <inheritdoc />
+    public PropertyCacheLevel GetDeliveryApiPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Elements;
+
+    /// <inheritdoc />
+    public Type GetDeliveryApiPropertyValueType(IPublishedPropertyType propertyType) => typeof(BlockListModel);
+
+    /// <inheritdoc />
+    public object? ConvertIntermediateToDeliveryApiObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview, bool expanding)
+    {
+        object? blocks = ConvertIntermediateToObject(owner, propertyType, referenceCacheLevel, inter, preview);
+
+        if (blocks is not BlockListModel blockList)
+        {
+            return null;
+        }
+
+        var enabledBlocks = blockList.Where(i => i.Settings?.IsVisible() ?? i.Content.IsVisible()).ToList();
+        if (enabledBlocks.Count == 0)
+        {
+            return null;
+        }
+
+        return new ApiBlockListModel(
+            enabledBlocks != null
+                ? enabledBlocks.Select(CreateApiBlockItem).ToArray()
+                : Array.Empty<ApiBlockItem>());
+    }
+
+    private ApiBlockItem CreateApiBlockItem(BlockListItem item) => new(
+            _apiElementBuilder.Build(item.Content),
+            item.Settings != null ? _apiElementBuilder.Build(item.Settings) : null);
 }
